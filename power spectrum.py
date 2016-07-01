affd34c94140e058d0c378dd42c24b1716b4cb73
Ann0000 <q.liang@uq.net.au>
2016-07-01 21:55:27 +1000

    I use np.frompyfunc rewrite the function and get the same result. But it's a quite simpler way to transform the result of math package to numpy array.

--- power spectrum.py
+++ power spectrum.py
@@ -4,7 +4,13 @@ import matplotlib.pyplot as pl
 from ed_functions_memo2 import sigma_v_function
 from ed_functions_memo2 import sigma_v_nonlinear
 z=np.arange(0.01,1.,0.01)
-sigma_v1=[]
+np_sigma=np.frompyfunc(sigma_v_function,3,1)
+sigma_v1=np_sigma(z,0.8,0.3)
+sigma_v2=np_sigma(z,0.6,0.3)
+pl.plot(z,sigma_v1,color='r')
+pl.plot(z,sigma_v2,color='b')
+pl.show()
+"""sigma_v1=[]
 sigma_v2=[]
 for z_i in range(1,100,1):
     sigma_v1_i=(-sigma_v_function(z_i/100.,0.8,0.3))
@@ -15,8 +21,12 @@ pl.plot(z,sigma_v1,color='r')
 pl.plot(z,sigma_v2,color='b')
 pl.ylabel('sigma_v1 & sigma_v2')
 pl.xlabel('redshift z')
-pl.show()
+pl.show()"""
+
+
+
 # The first try to draw plot sigma_v--z.
-# The next step is to
+# 21:52 write another function 
+# The next step is to vary sigma_8 and get the function [seems a quite hard job!
 
 
