840d05fa76b2a01919123f6c177de9f29964e91a
Ann0000 <q.liang@uq.net.au>
2016-07-09 09:21:20 +1000

    Here it is. The final (?) approximation and the contour with x_axis: omega_m & y_axis: sigma_8. I draw five figures with z from 0.2 to 0.4 by step 0.05. The approximation suits the original one best at z=0.3. It seems not work quite well at z=0.2. However, I can change that. For now, I just don't know which part of redshift are we mostly interested in? It works quite well at z=0.3. I will upload the zip of these five figures to GitHub then. The next I will do is run the emcee to test whether it's a good approximation.

--- power spectrum.py
+++ power spectrum.py
@@ -2,7 +2,6 @@ import cosmolopy
 import numpy as np
 import matplotlib.pyplot as pl
 from ed_functions_memo2 import sigma_v_function
-from ed_functions_cosmology2 import Ez
 
 
 np_sigma=np.frompyfunc(sigma_v_function,3,1)
@@ -17,45 +16,23 @@ def  sigma_approx(z,sigma_8,omega_m):
 
     return sigma_v
 
-"""z=0.2
-omega_m=20
-sigma_8=20
-i=0
-j=0
-sigma_v1=-np_sigma(z,0.2,0.1)
-for i in range(omega_m):
-    omega_m_i=0.2+0.01*i
-    for j in range(sigma_8):
-        sigma_8_j=j*0.15+0.1
-        sigma_v1_i_j=-np_sigma(z, sigma_8_j,omega_m_i )
-        sigma_v1.append(sigma_v1_i_j)
-        print(sigma_v1)
 
-#sigma_approx1 = sigma_approx(z, sigma_8, omega_m)
-#sigma_v1=np.array([(x*y) for x in x_axis for y in y_axis ])
-print(sigma_v1.size)
-sigma_v1=np.reshape(sigma_v1,(20,20))"""
 
-z=0.2
+z=0.35
 omega_m=np.arange(0.2,0.4,0.01)
-sigma_8=np.arange(0.,3.,0.1)
+sigma_8=np.arange(0.1,3.,0.1)
 sigma_8_j=0.1
 omega_m_i=0.2
-n_omega=omega_m.shape
-print(n_omega)
-n_sigma=sigma_8.shape
-print(np_sigma(z,sigma_8_j,omega_m_i))
-sigma_v=np.array([(sigma_8_j*omega_m_i)   for sigma_8_j in sigma_8 for omega_m_i in omega_m])
 
-sigma_v=sigma_v.reshape(30,20)
-
-
-
+sigma_8,omega_m=np.meshgrid(sigma_8,omega_m)
+sigma_v1=np_sigma(z,sigma_8,omega_m)
+sigma_approx1 = sigma_approx(z, sigma_8, omega_m)
 
 pl.ylabel('sigma_8')
 pl.xlabel('omega_m')
-pl.contour(omega_m,sigma_8,sigma_v)
-pl.title('z=0.2')
+pl.contour(omega_m,sigma_8,sigma_v1)
+pl.contour(omega_m,sigma_8,sigma_approx1)
+pl.title('z=0.35')
 pl.show()
 
 
